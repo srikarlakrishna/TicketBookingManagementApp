9eb967326f6695230c5766fe1da4e9d7c9ba65e9
krish <srikarlakrishna7@gmail.com>
2020-05-02 00:11:48 +0530

    app jenkin test

---
+++ src/main/java/com/infotech/book/ticket/app/TicketBookingManagementAppApplication.java
@@ -0,0 +1,12 @@
+package com.infotech.book.ticket.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingManagementAppApplication.class, args);
+	}
+}
